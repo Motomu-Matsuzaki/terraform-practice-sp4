9bcb82b9c8fbf13f1a01e55a3033a0657556a486
motomu.matsuzaki <motomu.matsuzaki@ctc-g.co.jp>
2025-07-30 00:33:55 +0900

    Apply terraform fmt to fix formatting issues

--- glue/main.tf
+++ glue/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "glue_service_role" {
-  name = "AWSGlueServiceRole-sprint3"
+  name        = "AWSGlueServiceRole-sprint3"
   description = "Allows Glue to call AWS services on your behalf. "
 
   assume_role_policy = jsonencode({
@@ -36,21 +36,21 @@ resource "aws_glue_job" "sprint3_customer_csv_normalize_parquet" {
     script_location = "s3://aws-glue-assets-593793020094-ap-northeast-1/scripts/sprint3-customer-csv-normalize-parquet.py"
   }
 
-  max_retries         = 0
-  timeout             = 480
+  max_retries = 0
+  timeout     = 480
   execution_property {
     max_concurrent_runs = 1
   }
 
   default_arguments = {
-    "--job-language"               = "python"
-    "--enable-metrics"            = "true"
-    "--enable-job-insights"       = "true"
-    "--enable-spark-ui"           = "true"
-    "--spark-event-logs-path"     = "s3://aws-glue-assets-593793020094-ap-northeast-1/sparkHistoryLogs/"
-    "--TempDir"                   = "s3://aws-glue-assets-593793020094-ap-northeast-1/temporary/"
-    "--job-bookmark-option"       = "job-bookmark-disable"
-    "--enable-glue-datacatalog"   = "true"
+    "--job-language"                 = "python"
+    "--enable-metrics"               = "true"
+    "--enable-job-insights"          = "true"
+    "--enable-spark-ui"              = "true"
+    "--spark-event-logs-path"        = "s3://aws-glue-assets-593793020094-ap-northeast-1/sparkHistoryLogs/"
+    "--TempDir"                      = "s3://aws-glue-assets-593793020094-ap-northeast-1/temporary/"
+    "--job-bookmark-option"          = "job-bookmark-disable"
+    "--enable-glue-datacatalog"      = "true"
     "--enable-observability-metrics" = "true"
   }
 }
